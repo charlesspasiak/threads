c0811cb56384d60a6006666b23b0e72cfa535346
charlespasiak <charlesspasiak@gmail.com>
2023-08-28 14:56:12 +0700

    implemented activiy page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -117,3 +117,32 @@ export async function fetchUsers({
     throw new Error(`Failed to fecth users: ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // find all threads created by user
+    const userThreads = await Thread.find({ author: userId });
+
+    // collect all the thread ids from the 'children' field
+
+    // collect all the child thread ids (replies) from 'children' field
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id',
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`);
+  }
+}
