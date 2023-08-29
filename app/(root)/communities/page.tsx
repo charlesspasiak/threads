347bf1694eb7472e4d588da99a475e8d4ba11ce5
charlespasiak <charlesspasiak@gmail.com>
2023-08-29 21:15:07 +0700

    implemented search and pagination

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,28 +1,33 @@
-import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
-import { profileTabs } from '@/constants';
-import Image from 'next/image';
-import UserCard from '@/components/cards/UserCard';
-import { fetchCommunities } from '@/lib/actions/community.actions';
+import Searchbar from '@/components/shared/Searchbar';
+import Pagination from '@/components/shared/Pagination';
 import CommunityCard from '@/components/cards/CommunityCard';
 
-const page = async () => {
-  const user = await currentUser();
+import { fetchUser } from '@/lib/actions/user.actions';
+import { fetchCommunities } from '@/lib/actions/community.actions';
 
+const page = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect('/onboarding');
 
-  // Fetch communities
-  const result = await fetchCommunities({ searchString: '', pageNumber: 1, pageSize: 25 });
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
   return (
     <section>
-      <h1 className="head-text mb-10">Community</h1>
+      <h1 className="head-text">Communities</h1>
+
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
 
       <div className="mt-14 flex flex-col gap-9">
         {result.communities.length === 0 ? (
@@ -43,6 +48,8 @@ const page = async () => {
           </>
         )}
       </div>
+
+      <Pagination path="communities" pageNumber={searchParams?.page ? +searchParams.page : 1} isNext={result.isNext} />
     </section>
   );
 };
